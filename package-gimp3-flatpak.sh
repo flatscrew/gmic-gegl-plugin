f000d8341af79f51e1a4438379553d99e63a0335
activey <activey@doaplatform.org>
2025-12-22 12:02:06 +0100

    Removed unnecessary link_args

--- package-gimp3-flatpak.sh
+++ package-gimp3-flatpak.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+VERSION="$(cat VERSION-RUNTIME)"
+
 flatpak-builder --force-clean --disable-rofiles-fuse --keep-build-dirs --repo=repo "$PWD/_install" org.gimp.GIMP.Plugin.GMICRuntime.yml
-flatpak build-bundle --runtime repo org.gimp.GIMP.Plugin.GMICRuntime.flatpak --runtime-repo=https://dl.flathub.org/repo/flathub.flatpakrepo org.gimp.GIMP.Plugin.GMICRuntime 3
+flatpak build-bundle --runtime repo org.gimp.GIMP.Plugin.GMICRuntime-$VERSION.flatpak --runtime-repo=https://dl.flathub.org/repo/flathub.flatpakrepo org.gimp.GIMP.Plugin.GMICRuntime 3
--- package-rasterflow-flatpak.sh
+++ package-rasterflow-flatpak.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+VERSION="$(cat VERSION-RUNTIME)"
+
 flatpak-builder --force-clean --disable-rofiles-fuse --keep-build-dirs --repo=repo "$PWD/_install" io.flatscrew.RasterFlow.Extension.GMICRuntime.yml
-flatpak build-bundle --runtime repo io.flatscrew.RasterFlow.Extension.GMICRuntime.flatpak --runtime-repo=https://dl.flathub.org/repo/flathub.flatpakrepo io.flatscrew.RasterFlow.Extension.GMICRuntime 1
+flatpak build-bundle --runtime repo io.flatscrew.RasterFlow.Extension.GMICRuntime-$VERSION.flatpak --runtime-repo=https://dl.flathub.org/repo/flathub.flatpakrepo io.flatscrew.RasterFlow.Extension.GMICRuntime 1
